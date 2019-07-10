de9a7712ec2d757674ac3283e728c875b4f2d0bd
Takaitech <takai.tech2144@gmail.com>
2019-07-09 17:54:18 -0700

    Updates

---
+++ precache-manifest.0cfcad803efa8b8e31b33dcb3168cc4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55be85f1b2d25703bffedd1dbb8b8164",
+    "url": "/to-do-list-reel/index.html"
+  },
+  {
+    "revision": "f676a165948a9c69749b",
+    "url": "/to-do-list-reel/static/css/main.d072503b.chunk.css"
+  },
+  {
+    "revision": "44892cb71b0935990dbf",
+    "url": "/to-do-list-reel/static/js/2.b838a073.chunk.js"
+  },
+  {
+    "revision": "f676a165948a9c69749b",
+    "url": "/to-do-list-reel/static/js/main.05da4da4.chunk.js"
+  },
+  {
+    "revision": "b41038da2ff63713b2e2",
+    "url": "/to-do-list-reel/static/js/runtime~main.1de3d311.js"
+  }
+]);
